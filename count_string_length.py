b0e74885f18241adac53d8c80d5a9646d29ca05c
Jeffreyy C.H. Yu <itsjeffreyy76@gmail.com>
2019-01-20 23:13:21 +0800

    The python 100 program start!
    The First python script is count string length

---
+++ count_string_length.py
@@ -0,0 +1,4 @@
+# author: Jeffreyy C.H. Yu
+# Note: Calculate the length of the string user entered.
+string = str(input("Please enter the string: "))
+print("The length of string is: ", len(string))
