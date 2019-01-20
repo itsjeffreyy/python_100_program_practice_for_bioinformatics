0a4cc15301cbd0c105d5142811bf75418e058519
Jeffrey Chun-Hui Yu <itsjeffreyy76@gmail.com>
2019-01-21 00:12:40 +0800

    modify the note and usage.

--- count_string_length.py
+++ count_string_length.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
 # author: Jeffreyy C.H. Yu
 # Note: Calculate the length of the string user entered.
+# usage: `python count_string_length.py` or `python count_string_length.py your_string`.
+#        The result shows the length of your string and your real string.
+
 import sys
 if len(sys.argv) >1:
     string = sys.argv[1]
 else:
-    string = str(input("Please enter the string: "))
+    string = str(input("Please enter your string: "))
 
 
 print(len(string), string)
