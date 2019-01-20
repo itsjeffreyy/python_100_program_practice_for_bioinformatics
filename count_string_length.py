01c0aa3707ba9fe0f8097a7eaecdcf8d9fd55ed6
Jeffrey Chun-Hui Yu <itsjeffreyy76@gmail.com>
2019-01-21 00:05:51 +0800

    To integrat the function of entering string from argv or input in one script

--- count_string_length.py
+++ count_string_length.py
@@ -1,4 +1,10 @@
 # author: Jeffreyy C.H. Yu
 # Note: Calculate the length of the string user entered.
-string = str(input("Please enter the string: "))
-print("The length of string is: ", len(string))
+import sys
+if len(sys.argv) >1:
+    string = sys.argv[1]
+else:
+    string = str(input("Please enter the string: "))
+
+
+print(len(string), string)
